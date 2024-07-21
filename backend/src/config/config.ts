62e747bfb640e12af487e2b7d63e69b0e53388f4
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2024-07-21 16:55:32 +0200

    remove console logs

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const MONGO_URL = process.env.MONGO_URL || "";
 const MONGO_DB = process.env.MONGO_DB || "";
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-console.log(process.env);
+
 export const config = {
   mongo: {
     url: MONGO_URL,
--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -4,7 +4,6 @@ import { config } from "./config";
 
 const connectDb = async () => {
   try {
-    console.log(config.mongo.url);
     await mongoose.connect(config.mongo.url, { dbName: config.mongo.db });
     console.log(`Database connected`);
   } catch (error) {
